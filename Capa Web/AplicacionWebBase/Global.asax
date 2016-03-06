62e879993b525f58177923670354753f64152171
Victor Cornejo <rex2002xp@gmail.com>
2016-03-06 00:07:49 -0600

    Organizacion de la solucion en capas

---
+++ Capa Web/AplicacionWebBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AplicacionWebBase.MvcApplication" Language="C#" %>
---
+++ Capa Web/AplicacionWebBase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacionWebBase.Startup))]
+namespace AplicacionWebBase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
