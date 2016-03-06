992896ec9fbde5fa55fb832d6fd1678f1c1b0da8
Victor Cornejo <rex2002xp@gmail.com>
2016-03-06 00:36:32 -0600

    Se creo la capa de datos para el manejo de identidades de usuarios y roles.

--- Capa Web/AplicacionWebBase/App_Start/Startup.Auth.cs
+++ Capa Web/AplicacionWebBase/App_Start/Startup.Auth.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using AplicacionWebBase.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using AplicacionWebBase.Models;
+using System;
 
 namespace AplicacionWebBase
 {
---
+++ Capa de Datos/IdentityBase/ApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityBase
+{
+    /// <summary>
+    /// Clase personalizada que hereda de IdentityUser, nos permite agregar los campos de Nombres , Nivel y Fecha de enrolamiento.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Aca podemos agregar el codigo para personalizar el claims del usuario.
+            return userIdentity;
+        }
+    }
+}
