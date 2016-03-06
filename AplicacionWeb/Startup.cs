670204174399e64dfb31b49ca3e4a155ca7492bf
Victor Cornejo <rex2002xp@gmail.com>
2016-03-05 23:27:23 -0600

    Creacion de Proyecto AspNet MVC 5

---
+++ AplicacionWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacionWeb.Startup))]
+namespace AplicacionWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
